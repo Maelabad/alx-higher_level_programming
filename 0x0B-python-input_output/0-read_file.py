3c9a0b1530c9ea6ba77966d13a3b3890a27c4bff
Adjobo Elie <120084291+Maelabad@users.noreply.github.com>
2024-02-06 20:34:50 +0000

    Add files via upload

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a read_file function"""
 
+
 def read_file(filename=""):
     """read file function"""
     with open(filename, 'r', encoding='utf-8') as file:
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """write_file function"""
     with open(filename, 'w', encoding='utf-8') as file:
-            file.write(text)
-            return len(text)
+        file.write(text)
+        return len(text)
